03a0a741416dcb7f4c957e74413f8eba62017ba4
Natanael  R. Rodigues <natanaelrodrigues@gmail.com>
2021-01-06 15:38:22 -0300

    Separação entre areas do sistema.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Site\HomeController as SiteHomeControler;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [SiteHomeControler::class,'index']);
+
+
+Route::prefix('painel')->group(Function(){
+    Route::get('/',[AdminHomeController::class,'index']);
 });
+
+
+Route::get('/authenticate',Function(){
+    echo 'Login do sistema';
+})->name('login');
