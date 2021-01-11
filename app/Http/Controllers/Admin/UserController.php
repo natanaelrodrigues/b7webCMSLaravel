8c901f4c4a023ce4e9beeea010122922d5cbaf93
Natanael R. Rodrigues <natanaelrodrigues@gmail.com>
2021-01-11 20:49:34 -0300

    Colocando bloqueio de autenticação de usuario.

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -13,6 +13,11 @@ use App\Models\User;
 
 class UserController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
